b694ca8e805680a28ce24bd7f870339f8384921f
inv-joseph <josephjohn@innovaturelabs.com>
2018-06-18 11:04:33 +0530

    initial project version

---
+++ Hello-World4/src/MyBatisInsert.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisInsert {
+
+	public static void main(String args[]) throws IOException{
+	      
+	      Reader reader = Resources.getResourceAsReader("MyBatisConf/SqlMapConfig.xml");
+	      SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);		
+	      SqlSession session = sqlSessionFactory.openSession();
+	      
+	      //Create a new student object
+	      Student student = new Student("Noorjahan","Electronics", 80, 984803322, "Noorjahan@gmail.com" ); 
+	            
+	      //Insert student data      
+	      session.insert("Student.insert", student);
+	      System.out.println("record inserted successfully");
+	      session.commit();
+	      session.close();			
+	}
+}
